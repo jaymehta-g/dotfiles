d77e2892b86df1c223c3f4b5a0d72fbc026cd499
Jay <91770194+jaymehta-g@users.noreply.github.com>
2024-01-15 17:48:22 -0600

    Added more required languages

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -3,6 +3,13 @@ return {
   opts = {
     ensure_installed = {
       "clangd",
+      "json-lsp",
+      "lua-language-server",
+      "markdownlint",
+      "marksman",
+      "shfmt",
+      "stylua",
+      "yaml-language-server",
     },
   },
 }
