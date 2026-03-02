596425b99972c3c8021c5dc29bbce1e951b93cb6
Jay <91770194+jaymehta-g@users.noreply.github.com>
2026-03-02 17:13:30 -0600

    godot cmp, repo project thing

--- .config/godot/script_templates/Node/component.gd
+++ .config/godot/script_templates/Node/component.gd
@@ -1,10 +1,13 @@
-class_name ClassName
-extends Node
-
 #region Component boilerplate
+@export var actor: Node
+@onready var __discard = __cmp_onready()
 const _cmp_key = &"ClassName"
-func attach(actor: Node):
-	actor.set_meta(_cmp_key, self)
+func __cmp_onready():
+	if not actor: actor = get_parent()
+	attach(actor)
+	return actor
+func attach(n: Node):
+	n.set_meta(_cmp_key, self)
 static func from(n: Node) -> ClassName:
 	if n.has_meta(_cmp_key):
 		return n.get_meta(_cmp_key)
